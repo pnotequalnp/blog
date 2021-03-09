0f11080f7f6a4dd5e985fc472f24c0df7bbd3d04
Kevin Mullins <kevin@pnotequalnp.com>
2021-03-09 11:31:42 -0800

    Provide `SyntaxHighlighter` element to MDX posts

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -8,12 +8,16 @@ import { Container, Header } from 'semantic-ui-react';
 import Head from 'next/head';
 import hydrate from 'next-mdx-remote/hydrate';
 import renderToString from 'next-mdx-remote/render-to-string';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
+const components: MdxRemote.Components = {
+  SyntaxHighlighter
+};
 
 export type Props = Post<MdxRemote.Source>;
 
-export const BlogPost: FC<Props> = ({ metadata, content }) => {
-
-  return <>
+export const BlogPost: FC<Props> = ({ metadata, content }) =>
+  <>
     <Head>
       <title>{metadata.title}</title>
     </Head>
@@ -23,10 +27,9 @@ export const BlogPost: FC<Props> = ({ metadata, content }) => {
         <Header.Subheader>{metadata.date}</Header.Subheader>
         <Header.Subheader>{metadata.summary}</Header.Subheader>
       </Header>
-      <Container text as='article' textAlign='left'>{hydrate(content)}</Container>
+      <Container text as='article' textAlign='left'>{hydrate(content, { components })}</Container>
     </Container>
   </>;
-};
 
 export default BlogPost;
 
@@ -45,7 +48,7 @@ export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
   const posts = await getPosts(path.join(process.cwd(), '/src/posts'));
   const postSource = posts.find(post => post.metadata.id === params.id);
   if (postSource === undefined) return { notFound: true };
-  const content = await renderToString(postSource.content);
+  const content = await renderToString(postSource.content, { components });
   const post = { ...postSource, content };
   return {
     props: post
