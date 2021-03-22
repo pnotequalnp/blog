78708129bca56d8af3f8093cf76e48cdd662eb17
Kevin Mullins <kevin@pnotequalnp.com>
2021-03-21 18:04:28 -0700

    Fill some content on `index` and `about`

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,7 +5,7 @@ import type { MdxRemote } from 'next-mdx-remote/types';
 import Head from 'next/head';
 import { pinnedRepos } from '../lib/github';
 import GitHubRepo from '../components/GitHubRepo';
-import { Card, Container, Header } from 'semantic-ui-react'
+import { Card, Container, Divider, Header } from 'semantic-ui-react'
 import renderToString from 'next-mdx-remote/render-to-string';
 
 export type Props = {
@@ -18,6 +18,38 @@ export const About: FC<Props> = ({ repos }) => {
     <Head>
       <title>Kevin Mullins - About</title>
     </Head>
+    <Container as='section' text>
+      <Header as='h1' content='About Me' />
+      <p>
+        I am a university student studying math and computer science. I am a programmer by trade,
+        and some of my work can be found on my GitHub which is linked below, along with my pinned
+        repos. I am graduating in the near future and am looking for a position for when that
+        happens.
+      </p>
+      <Header as='h2' content='Interests' />
+      <p>
+        One of my biggest interests is software architecture and engineering. I'm also interested in
+        the implementation and application of statically typed purely functional programming
+        langauges. Additionally I enjoy the application of math to solve problems in programming,
+        especially graph theory, category theory, and type theory. I also follow the world of
+        security a little bit.
+      </p>
+      <Header as='h2' content='Technologies' />
+      <p>
+        I'm something of a polyglot, I am very comfortable working with a wide range of programming
+        languages. The languages I enjoy writing the most are Haskell and Rust. I currently daily
+        drive NixOS, a Linux distribution based on the Nix package manager, which is a purely
+        functional package manager with deterministic and reproducible builds. I have worked with
+        both MacOS and Windows in the past.
+      </p>
+      <Header as='h2' content='Other Interests' />
+      <p>
+        Outside the technical world, I am involved with music, like to cook, play chess, am learning
+        French, and enjoy solving Rubik's cube style puzzles. You can find software on these topics
+        that I've written on my GitHub.
+      </p>
+    </Container>
+    <Divider section />
     <Container as='section'>
       <Header as='h1' content='Pinned GitHub Repos' textAlign='center' />
       <Card.Group content={cards} centered />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import type { FC } from 'react';
+import { Header } from 'semantic-ui-react';
 
 export const Home: FC<{}> = () =>
   <>
+    <Header as='h1' content='Welcome!' />
   </>
 
 export default Home;
