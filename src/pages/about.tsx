e6254bf88a19b644ff8b1ec7f5dab011344cfdbf
Kevin Mullins <kevin@pnotequalnp.com>
2021-03-29 02:05:10 -0700

    Reorganize `about` and `index`

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,38 +1,29 @@
 import type { FC } from 'react';
-import type { GetStaticProps } from 'next'
-import type { Repo } from '../lib/github';
-import type { MdxRemote } from 'next-mdx-remote/types';
 import Head from 'next/head';
-import { pinnedRepos } from '../lib/github';
-import GitHubRepo from '../components/GitHubRepo';
-import { Card, Container, Divider, Header } from 'semantic-ui-react'
-import renderToString from 'next-mdx-remote/render-to-string';
+import { Container, Header } from 'semantic-ui-react'
 
-export type Props = {
-  repos: Repo<MdxRemote.Source | null>[]
-};
-
-export const About: FC<Props> = ({ repos }) => {
-  const cards = repos.map(repo => <GitHubRepo key={repo.id} {...repo} />);
-  return <>
+export const About: FC<{}> = () =>
+  <>
     <Head>
       <title>Kevin Mullins - About</title>
     </Head>
     <Container as='section' text>
       <Header as='h1' content='About Me' />
       <p>
-        I am a university student studying math and computer science. I am a programmer by trade,
-        and some of my work can be found on my GitHub which is linked below, along with my pinned
-        repos. I am graduating in the near future and am looking for a position for when that
-        happens.
+        I am a university undergraduate studying math and computer science.  One of my biggest
+        interests is software architecture and engineering. I'm also interested in the
+        implementation and application of statically typed purely functional programming langauges.
+        Additionally I enjoy the application of math such as graph theory, category theory, type
+        theory, and abstract algebra to solve difficult problems in programming. I also follow the
+        world of security a little bit.
       </p>
-      <Header as='h2' content='Interests' />
+      <Header as='h2' content='Experience' />
       <p>
-        One of my biggest interests is software architecture and engineering. I'm also interested in
-        the implementation and application of statically typed purely functional programming
-        langauges. Additionally I enjoy the application of math such as graph theory, category
-        theory, type theory, and abstract algebra to solve difficult problems in programming. I
-        also follow the world of security a little bit.
+        The summer of 2019 I interned for Aon IPS, the intellectual property division of the
+        multinational professional insurance and risk-mitigation company. There I researched,
+        designed, and implemented a system to aggregate data from various sources, construct and
+        analyze large graphs to extract relevant domain-specific information using natural language
+        processing techniques, and package that data to be consumed by other internal services.
       </p>
       <Header as='h2' content='Technologies' />
       <p>
@@ -49,24 +40,6 @@ export const About: FC<Props> = ({ repos }) => {
         topics that I've written on my GitHub.
       </p>
     </Container>
-    <Divider section />
-    <Container as='section'>
-      <Header as='h1' content='Pinned GitHub Repos' textAlign='center' />
-      <Card.Group content={cards} centered />
-    </Container>
   </>;
-};
 
 export default About;
-
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const repoSources = await pinnedRepos('pnotequalnp');
-  const repos = await Promise.all(repoSources.map(async repo => {
-    const readme = repo.readme ? await renderToString(repo.readme ?? '') : null;
-    return { ...repo, readme };
-  }));
-  return {
-    props: { repos },
-    revalidate: 60
-  };
-};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,45 @@
 import type { FC } from 'react';
-import { Header } from 'semantic-ui-react';
+import type { GetStaticProps } from 'next';
+import type { MdxRemote } from 'next-mdx-remote/types';
+import renderToString from 'next-mdx-remote/render-to-string';
+import { Card, Container, Header } from 'semantic-ui-react'
+import { pinnedRepos, Repo } from '../lib/github';
+import GitHubRepo from '../components/GitHubRepo';
 
-export const Home: FC<{}> = () =>
-  <>
-    <Header as='h1' content='Welcome!' />
+export type Props = {
+  repos: Repo<MdxRemote.Source | null>[]
+};
+
+export const Home: FC<Props> = ({ repos }) => {
+  const cards = repos.map(repo => <GitHubRepo key={repo.id} {...repo} />);
+
+  return <>
+    <Header id='name' as='h1' content='Kevin Mullins' />
+    <Container as='section' style={{ flexGrow: 1 }}>
+      <p style={{ fontSize: '1.4rem' }}>
+        I am a software developer with an interest in the design and implementation of scalable
+        and distributed software systems and the application of new and existing mathematical
+        knowledge bases to the design and implementation of software and software development tools.
+        I am currently a university undergraduate studying math and computer science.
+      </p>
+    </Container>
+    <Container as='section'>
+      <Header as='h1' content='Pinned GitHub Repos' textAlign='center' />
+      <Card.Group content={cards} centered />
+    </Container>
   </>
+};
 
 export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const repoSources = await pinnedRepos('pnotequalnp');
+  const repos = await Promise.all(repoSources.map(async repo => {
+    const readme = repo.readme ? await renderToString(repo.readme ?? '') : null;
+    return { ...repo, readme };
+  }));
+  return {
+    props: { repos },
+    revalidate: 60
+  };
+};
